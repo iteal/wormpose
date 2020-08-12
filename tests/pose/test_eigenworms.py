4dc9a66c4d13bd0f80e564510e3e97558e22745b
iteal <laetitia.hebert@oist.jp>
2020-08-12 16:40:29 +0900

    add more tests for eigenworms theta modes conversion

--- tests/pose/test_eigenworms.py
+++ tests/pose/test_eigenworms.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import pytest
 
-from wormpose.pose.eigenworms import load_eigenworms_matrix
+from wormpose.pose.eigenworms import load_eigenworms_matrix, modes_to_theta, theta_to_modes
 
 _EIGENWORMS_FILE_PATH = "extras/EigenWorms.csv"
 
@@ -37,3 +37,39 @@ def test_load_none():
 def test_load_invalid_path():
     with pytest.raises(OSError):
         load_eigenworms_matrix("invalid_path")
+
+
+def test_modes_to_theta():
+    eigenworms_matrix = load_eigenworms_matrix(_EIGENWORMS_FILE_PATH)
+
+    dims = eigenworms_matrix.shape[1]
+
+    input_outputs = [
+        (np.full(dims, np.nan), np.full(dims, np.nan)),
+        (np.zeros(dims, dtype=float), np.zeros(dims, dtype=float)),
+        (np.array([1] + [0] * (dims - 1)).astype(float), eigenworms_matrix[:, 0]),
+        (np.array([0] * (dims - 1) + [-10]).astype(float), eigenworms_matrix[:, -1] * -10),
+        (np.array([3] + [0] * (dims - 1)).astype(float), eigenworms_matrix[:, 0] * 3),
+    ]
+
+    for input, expected_output in input_outputs:
+        output = modes_to_theta(input, eigenworms_matrix)
+        assert np.allclose(output, expected_output, atol=1e-3, equal_nan=True)
+
+
+def test_theta_to_modes():
+    eigenworms_matrix = load_eigenworms_matrix(_EIGENWORMS_FILE_PATH)
+
+    dims = eigenworms_matrix.shape[1]
+
+    input_outputs = [
+        (np.full(dims, np.nan), np.full(dims, np.nan)),
+        (np.zeros(dims, dtype=float), np.zeros(dims, dtype=float)),
+        (np.ones(dims, dtype=float), np.zeros(dims, dtype=float)),
+        (eigenworms_matrix[:, 0], np.array([1] + [0] * (dims - 1)).astype(float)),
+        (eigenworms_matrix[:, 1] * 2, np.array([0, 2] + [0] * (dims - 2)).astype(float)),
+    ]
+
+    for input, expected_output in input_outputs:
+        output = theta_to_modes(input, eigenworms_matrix)
+        assert np.allclose(output, expected_output, atol=1e-3, equal_nan=True)
